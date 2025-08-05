cf5f5987b60b4d07e141152adced298532624b5e
luo1imasi <2488288779@qq.com>
2025-08-05 14:11:43 +0800

    add feet raycaster

--- legged_lab/utils/env_utils/scene.py
+++ legged_lab/utils/env_utils/scene.py
@@ -72,6 +72,23 @@ class SceneCfg(InteractiveSceneCfg):
             ),
         )
 
+        self.left_feet_scanner = RayCasterCfg(
+            prim_path="{ENV_REGEX_NS}/Robot/left_ankle_roll_link",
+            offset=RayCasterCfg.OffsetCfg(pos=(0.03, 0.0, 20.0)),
+            attach_yaw_only=True,
+            pattern_cfg=patterns.GridPatternCfg(resolution=0.05, size=[0.15, 0.05]),
+            debug_vis=True,
+            mesh_prim_paths=["/World/ground"],
+        )
+        self.right_feet_scanner = RayCasterCfg(
+            prim_path="{ENV_REGEX_NS}/Robot/right_ankle_roll_link",
+            offset=RayCasterCfg.OffsetCfg(pos=(0.03, 0.0, 20.0)),
+            attach_yaw_only=True,
+            pattern_cfg=patterns.GridPatternCfg(resolution=0.05, size=[0.15, 0.05]),
+            debug_vis=True,
+            mesh_prim_paths=["/World/ground"],
+        )
+
         if config.height_scanner.enable_height_scan:
             self.height_scanner = RayCasterCfg(
                 prim_path="{ENV_REGEX_NS}/Robot/" + config.height_scanner.prim_body_name,
