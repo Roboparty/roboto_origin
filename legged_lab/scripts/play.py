6ecd1ca32e229f84f4dbd4322ca0990a236fba3e
luo1imasi <2488288779@qq.com>
2025-08-06 17:00:04 +0800

    update play, add more function

--- legged_lab/scripts/play.py
+++ legged_lab/scripts/play.py
@@ -26,6 +26,9 @@ parser = argparse.ArgumentParser(description="Train an RL agent with RSL-RL.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
+parser.add_argument("--plane", action='store_true', help="Use plane terrain")
+parser.add_argument("--push_robot", action='store_true', help="Push robot during playing")
+parser.add_argument("--keyboard", action='store_true', help="Keyboard control mode")
 
 # append RSL-RL cli arguments
 cli_args.add_rsl_rl_args(parser)
@@ -39,6 +42,7 @@ simulation_app = app_launcher.app
 
 from isaaclab_rl.rsl_rl import export_policy_as_jit, export_policy_as_onnx
 from isaaclab_tasks.utils import get_checkpoint_path
+from isaaclab.devices import Se2Keyboard
 
 from legged_lab.envs import *  # noqa:F401, F403
 from legged_lab.utils.cli_args import update_rsl_rl_cfg
@@ -52,17 +56,30 @@ def play():
     env_cfg, agent_cfg = task_registry.get_cfgs(env_class_name)
 
     env_cfg.noise.add_noise = False
-    env_cfg.domain_rand.events.push_robot = None
+    if not args_cli.push_robot:
+        env_cfg.domain_rand.events.push_robot = None
     env_cfg.scene.max_episode_length_s = 40.0
     env_cfg.scene.num_envs = 50
     env_cfg.scene.env_spacing = 2.5
-    env_cfg.commands.ranges.lin_vel_x = (0.6, 0.6)
-    env_cfg.commands.ranges.lin_vel_y = (0.0, 0.0)
-    env_cfg.commands.ranges.heading = (0.0, 0.0)
-    env_cfg.scene.height_scanner.drift_range = (0.0, 0.0)
 
-    # env_cfg.scene.terrain_generator = None
-    # env_cfg.scene.terrain_type = "plane"
+    kbd_control = None
+    if args_cli.keyboard:
+        env_cfg.commands.heading_command=False
+        env_cfg.commands.rel_standing_envs = 0.0
+        kbd_control = Se2Keyboard(
+            v_x_sensitivity=env_cfg.commands.ranges.lin_vel_x[1], 
+            v_y_sensitivity=env_cfg.commands.ranges.lin_vel_y[1], 
+            omega_z_sensitivity=env_cfg.commands.ranges.ang_vel_z[1]
+        )
+    else:
+        env_cfg.commands.ranges.lin_vel_x = (0.6, 0.6)
+        env_cfg.commands.ranges.lin_vel_y = (0.0, 0.0)
+        env_cfg.commands.ranges.heading = (0.0, 0.0)
+        env_cfg.scene.height_scanner.drift_range = (0.0, 0.0)
+
+    if args_cli.plane:
+       env_cfg.scene.terrain_generator = None
+       env_cfg.scene.terrain_type = "plane"
 
     if env_cfg.scene.terrain_generator is not None:
         env_cfg.scene.terrain_generator.num_rows = 5
@@ -104,6 +121,12 @@ def play():
     obs, _ = env.get_observations()
 
     while simulation_app.is_running():
+        if kbd_control is not None:
+            vel_command = kbd_control.advance()
+            if hasattr(env, 'command_generator'):
+                env.command_generator.command[:, 0] = vel_command[0]  # lin_vel_x
+                env.command_generator.command[:, 1] = vel_command[1]  # lin_vel_y
+                env.command_generator.command[:, 2] = vel_command[2]  # ang_vel_z
 
         with torch.inference_mode():
             actions = policy(obs)
